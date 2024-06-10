fdbc012a37a66d20acab630063d9be201c8fa021
uw1yks <152063495+uw1yks@users.noreply.github.com>
2024-06-10 22:31:15 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   favicon: "img/favicon.svg", // https://dev.to/masakudamatsu/favicon-nightmare-how-to-maintain-sanity-3al7
 
   // Set the production url of your site here
-  url: "https://uw1yks.github.io/unraid-docs/",
+  url: "https://uw1yks.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/unraid-docs/",
